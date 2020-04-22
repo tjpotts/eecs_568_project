86df32614caecf62eb869aed9f90f3c8dc8daa26
TJ Potts <tjpotts5@gmail.com>
2020-04-22 17:19:43 -0400

    SLAM implementation (no GPS)

--- matlab/lib/LocalizerSlam.m
+++ matlab/lib/LocalizerSlam.m
@@ -1,35 +1,50 @@
-odometry_index gtsam.*
 
-classdef LocalizerSlam
+classdef LocalizerSlam < handle
     properties
+        % ISAM2 optimizer
+        isam
+
+        % Objects to hold new factors to add to the factor graph and their initial estimates
+        new_factors
+        initial_estimates
+
+        % Index and timestamp of the last pose added to the factor graph
+        pose_index = 0
+        pose_t = 0
+
+        % Holds a list of already observed landmarks and their semantic types
+        % landmarks(n,:) = [landmark_id, landmark_type]
+        landmarks = zeros(0,2);
+
+        % TODO: Create noise models
+        odometry_noise
+        gps_noise
+        landmark_noise
+
+        % Holds the output from ISAM
+        result
     end
+
     methods
         % Initialize the SLAM algorithm
         % initialPose: SE2 pose
         function initialize(obj)
-            % ISAM2 optimizer
-            obj.isam = ISAM2;
+            import gtsam.*
 
-            % Objects to hold new factors and initial estimates added to the factor graph
-            obj.new_factors = NonlinearFactorGraph;
-            obj.initial_estimates = Values;
-
-            % Index and timestamp of the last pose added to the factor graph
-            obj.pose_index = 0;
-            obj.pose_t = 0;
-
-            % Matrix of already observered landmarks and their semantic types
-            % landmarks(n,:) = [landmark_id, landmark_type]
-            obj.landmarks = [];
-
-            % Set noise parameters
-            % TODO: Create noise models
-            obj.odometry_noise = [];
-            obj.gps_noise = [];
-            obj.landmark_noise = [];
+            obj.isam = ISAM2();
+            obj.new_factors = NonlinearFactorGraph();
+            obj.initial_estimates = Values();
 
             % Create a prior on the origin
-            obj.new_factors.add(PriorFactorPose2("O",Pose2(0,0,0),noiseModel.Diagonal.Sigmas([1e-5 1e-5 1e-5])));
+            obj.new_factors.add(PriorFactorPoint2(symbol('O',0), Point2(0,0), noiseModel.Diagonal.Sigmas([1e-5; 1e-5])));
+            obj.initial_estimates.insert(symbol('O',0), Point2(0,0));
+            obj.new_factors.add(PriorFactorPose2(0, Pose2(0,0,pi), noiseModel.Diagonal.Sigmas([1e-5; 1e-5; 1e-5])));
+            obj.initial_estimates.insert(0, Pose2(0,0,0));
+
+            % Create noise models
+            obj.odometry_noise = noiseModel.Diagonal.Sigmas([1; 1; 1]);
+            obj.gps_noise = noiseModel.Diagonal.Sigmas([1; 1]);
+            obj.landmark_noise = noiseModel.Diagonal.Sigmas([1; 1; 1]);
         end
 
         % Adds a new odometry measurement to the factor graph
@@ -40,12 +55,12 @@ classdef LocalizerSlam
 
             % Add odometry factor the the factor graph
             i = obj.pose_index + 1; % Index for new pose
-            odometry_pose = Pose2(meas(1)*dt, meas(2)*dt, meas(3)*dt);
-            odometry_factor = BetweenFactorPose(i-1, i, odometry_pose, obj.odometry_noise);
+            odometry_pose = gtsam.Pose2(meas(1)*dt, meas(2)*dt, meas(3)*dt);
+            odometry_factor = gtsam.BetweenFactorPose2(i-1, i, odometry_pose, obj.odometry_noise);
             obj.new_factors.add(odometry_factor);
 
             % Calculate initial estimate of new pose based on previous estimate and odometry
-            current_pose = obj.result.getPose2At(i-1);
+            current_pose = obj.isam.calculateEstimatePose2(obj.pose_index);
             new_pose_initial = current_pose.compose(odometry_pose);
             obj.initial_estimates.insert(i,new_pose_initial);
 
@@ -61,8 +76,11 @@ classdef LocalizerSlam
             % TODO: Incorporate t and last odometry measurement into factor to account for distance travelled
             %       since corresponding pose was added?
             % Create the GPS factor and add it to the factor graph
-            gps_point = Point2(meas(1),meas(2));
-            gps_factor = DeltaFactor("O", obj.pose_index, gps_point, gps_noise);
+            display("GPS");
+            display(obj.pose_index);
+            display(meas);
+            gps_point = gtsam.Point2(meas(1),meas(2));
+            gps_factor = gtsam.DeltaFactor(obj.pose_index, gtsam.symbol('O',0), gps_point, obj.gps_noise);
             obj.new_factors.add(gps_factor);
         end
 
@@ -70,11 +88,13 @@ classdef LocalizerSlam
         % t: timestamp of measurement
         % meas: [Nx4] data of landmark observations at current timestep
         % meas(n,:) [ID,X,Y,Type] data for landmark observation n
-        function addLandmarks(obj, t, meas)
+        function addLandmark(obj, t, meas)
+            import gtsam.*
+
             % TODO: Incorporate t and alst odometry measurement into factor to account for distance travelled
             %       since corresponding pose was added?
             % Add the landmark observation factor to the pose graph
-            landmark_id = "L"+meas(1);
+            landmark_id = symbol('L',meas(1));
             landmark_point = Point2(meas(2),meas(3));
             landmark_factor = DeltaFactor(obj.pose_index, landmark_id, landmark_point, obj.landmark_noise);
 
@@ -83,7 +103,7 @@ classdef LocalizerSlam
                 % Add the landmark to the list of observerd landmarks
                 obj.landmarks = [obj.landmarks; meas(1) meas(4)];
                 % Calculate the initial estimate based on the current pose estimate
-                current_pose = obj.result.getpose2At(i);
+                current_pose = obj.isam.calculateEstimatePose2(obj.pose_index);
                 landmark_initial = current_pose.transformFrom(landmark_point);
                 obj.initial_estimates.insert(landmark_id, landmark_initial);
             end
@@ -91,41 +111,52 @@ classdef LocalizerSlam
 
         % Run the optimizer
         function optimize(obj)
-            % TODO: Will probably need to avoid running until we have enough information to 
+            display("Optimize");
+            import gtsam.*
+
+            % TODO: Will probably need to avoid running until we have enough information to
             %       have a well-defined linear system
             % Run ISAM2
-            isam.update(obj.new_factors,obj.initial_estimates)
-            obj.result = obj.isam.calculateEstimate()
+            obj.isam.update(obj.new_factors,obj.initial_estimates);
 
             % Reset the new_factors and initial_estimates objects
-            obj.new_factors = NonlinearFactorGraph
-            obj.initial_estimates = Values;
+            obj.new_factors.resize(0);
+            obj.initial_estimates.clear();
         end
 
         % Get the current pose estimate of the vehicle
         % returns: SE2 pose
         function pose = getPoseEstimate(obj)
-            gtPose = obj.result.getPose2At(obj.odometryIndex)
+            import gtsam.*
+
+            pose = obj.isam.calculateEstimatePose2(obj.pose_index);
             % TODO: Convert from gtsam type to 3x3 matrix
-            gtpose = zeros(3,3); % Temporary
+            %gtpose = zeros(3,3); % Temporary
         end
 
         % Get the current estimated trajectory of the vehicle
         % returns: [Nx3x3] matrics of stacked SE2 poses
         function traj = getTrajectoryEstimate(obj)
+            import gtsam.*
+
             % TODO: Implement
+            traj = [];
         end
 
         % Get the current estimate of the landmark map
         % returns: Nx3 matrix of landmark estimates
         % map(n,:) = [X,Y,Type] for landmark n
         function map = getMapEstimate(obj)
+            import gtsam.*
+
             % Generate the landmark map
             landmark_count = size(obj.landmarks,1);
             map = zeros(landmark_count,4);
             for i = 1:landmark_count
-                [landmark_id, landmark_type] = obj.landmarks(i,:);
-                landmark_point = obj.results.getPoint2At("L"+landmark_id);
+                landmark_id = obj.landmarks(i,1);
+                landmark_type = obj.landmarks(i,2);
+                results = obj.isam.calculateEstimate();
+                landmark_point = results.atPoint2(symbol('L',landmark_id));
                 map(i,:) = [landmark_id, landmark_point.x, landmark_point.y, landmark_type];
             end
         end
--- matlab/main.m
+++ matlab/main.m
@@ -11,47 +11,182 @@
 
 % TODO: Add input data listed above
 
-slam = LocalizerSlam;
-slam.initialize();
+clear;
 
-done = 0;
-t = 0;
-while(!done)
-    % Add odometry measurements at the current time
-    i_odometry = find(t_odometry == t)
-    if (size(i_odometry,2) > 0)
-        slam.addOdometry(odometry(i_odometry))
+START_TIME = 780;
+END_TIME = 1500;
+
+% Load vehicle data
+load('VehicleData3.mat');
+addpath('./lib');
+
+t_odometry = testListArray.Odometry.YawRateTime;
+odometry = zeros(numel(t_odometry),1);
+odometry_index = 1;
+for i = 1:numel(t_odometry)
+    t = t_odometry(i);
+    if (t < testListArray.Odometry.VehicleSpeedTime(1))
+        odometry(i,1) = testListArray.Odometry.VehicleSpeed(1);
+    elseif (t > testListArray.Odometry.VehicleSpeedTime(end))
+        odometry(i,1) = testListArray.Odometry.VehicleSpeed(end);
+    else
+        while (t > testListArray.Odometry.VehicleSpeedTime(odometry_index+1))
+            odometry_index = odometry_index+1;
+        end
+        t1 = testListArray.Odometry.VehicleSpeedTime(odometry_index);
+        t2 = testListArray.Odometry.VehicleSpeedTime(odometry_index+1);
+        v1 = testListArray.Odometry.VehicleSpeed(odometry_index);
+        v2 = testListArray.Odometry.VehicleSpeed(odometry_index+1);
+        slope = (v2 - v1) / (t2 - t1);
+        odometry(i,1) = v1 + slope * (t - t1);
     end
 
-    % Add gps measurements at the current time
-    i_gps = find(t_gps == t)
-    if (size(i_gps,2) > 0)
-        slam.addOdometry(gps(i_gps))
+    odometry(i,1) = odometry(i,1) * 0.27777;
+
+    % Rotational velocity (converted from deg/s to rad/s)
+    %odometry(i,3) = testListArray.Odometry.YawRate(i) * pi / 180;
+    odometry(i,3) = testListArray.Odometry.YawRate(i);
+    %odometry(i,3) = testListArray.Odometry.YawRate(i) * 180 / pi;
+end
+
+t_gps = testListArray.GPS.Time;
+gps = zeros(numel(t_gps),2);
+i_gps = find(t_gps > START_TIME, 1);
+lat0 = testListArray.GPS.Lat(i_gps);
+lon0 = testListArray.GPS.Long(i_gps);
+gps(:,2) = (testListArray.GPS.Lat - lat0) .* (pi / 180 * 6378100);
+gps(:,1) = -(testListArray.GPS.Long - lon0) .* (pi / 180 * 6378100) .* cos(lat0);
+for i = 1:numel(t_gps)
+    gps(i,1) = distance(lat0,lon0,lat0,testListArray.GPS.Long(i)) * (pi / 180 * 6378100);
+    if (testListArray.GPS.Long(i) < lon0)
+        gps(i,1) = -gps(i,1);
+    end
+    gps(i,2) = distance(lat0,lon0,testListArray.GPS.Lat(i),lon0) * (pi / 180 * 6378100);
+    if (testListArray.GPS.Lat(i) < lat0)
+        gps(i,2) = -gps(i,2);
+    end
+end
+
+landmark_count = min([
+    size(testListArray.Landmark.Time,1),
+    size(testListArray.Landmark.ID,1),
+    size(testListArray.Landmark.X,1),
+    size(testListArray.Landmark.Y,1),
+    size(testListArray.Landmark.Type,1)
+]);
+
+t_landmarks = testListArray.Landmark.Time(1:landmark_count,:);
+t_landmarks = reshape(t_landmarks,[numel(t_landmarks),1]);
+
+landmarks = zeros(numel(t_landmarks),4);
+landmarks(:,1) = reshape(testListArray.Landmark.ID(1:landmark_count,:), [numel(t_landmarks),1]);
+landmarks(:,2) = reshape(testListArray.Landmark.X(1:landmark_count,:), [numel(t_landmarks),1]);
+landmarks(:,3) = reshape(testListArray.Landmark.Y(1:landmark_count,:), [numel(t_landmarks),1]);
+landmarks(:,4) = reshape(testListArray.Landmark.Type(1:landmark_count,:), [numel(t_landmarks),1]);
+
+% Sort by time
+[t_landmarks, landmarks_sorted_index] = sort(t_landmarks);
+landmarks = landmarks(landmarks_sorted_index,:);
+
+% Filter out empty landmark measurements
+landmarks_filtered_index = landmarks(:,1) > 0;
+t_landmarks = t_landmarks(landmarks_filtered_index);
+landmarks = landmarks(landmarks_filtered_index,:);
+
+% Create unique ids
+landmark_id_times = zeros(0,3);
+next_landmark_id = max(landmarks(:,1)) + 1
+for i = 1:numel(t_landmarks)
+    id = landmarks(i,1);
+    idx = find(landmark_id_times(:,1) == id,1);
+    if (numel(idx) == 0)
+        % First time seeing landmark
+        landmark_id_times = [landmark_id_times; id t_landmarks(i) id];
+    elseif (t_landmarks(i) - landmark_id_times(idx,2) < 10)
+        % Consider this the same landmark as previously seen
+        landmark_id_times(idx,2) = t_landmarks(i);
+        landmarks(i,1) = landmark_id_times(idx,3);
+    else
+        % Consider this a new landmark
+        landmark_id_times(idx,2) = t_landmarks(i);
+        landmark_id_times(idx,3) = next_landmark_id;
+        landmarks(i,1) = next_landmark_id;
+        next_landmark_id = next_landmark_id + 1;
+    end
+end
+
+% Initialization
+
+slam = LocalizerSlam();
+slam.initialize();
+
+% Main loop
+
+done = 0;
+t = START_TIME;
+figure(1);
+axis equal;
+hold on;
+trajectory_line = animatedline('Color','b');
+gps_line = animatedline('Color','r');
+local_map_scatter = scatter([],[]);
+
+i_odometry = find(t_odometry > START_TIME, 1);
+i_gps = find(t_gps > START_TIME, 1);
+i_landmarks = find(t_landmarks > START_TIME, 1);;
+
+tic;
+t_plot_update = toc;
+while(~done)
+    display(t);
+
+    % Add odometry measurements at the current time
+    if (t >= t_odometry(i_odometry))
+        slam.addOdometry(t_odometry(i_odometry), odometry(i_odometry,:));
+        i_odometry = i_odometry + 1;
+    end
+
+    % Add GPS measurements at the current time
+    if (t >= t_gps(i_gps))
+        %slam.addGps(t_gps(i_gps), gps(i_gps,:));
+        addpoints(gps_line, gps(i_gps,1), gps(i_gps,2));
+        i_gps = i_gps + 1;
     end
 
     % Add landmark observations at the current time
-    i_odometry = find(t_odometry == t)
-    if (size(i_odometry,2) > 0)
-        slam.addOdometry(odometry(i_odometry))
+    if (t >= t_landmarks(i_landmarks))
+        slam.addLandmark(t_landmarks(i_landmarks), landmarks(i_landmarks,:));
+        i_landmarks = i_landmarks + 1;
     end
 
     % Run optimization
-    slam.optimize()
+    slam.optimize();
 
-    % Get results
-    local_map = slam.getMapEstimate()
-    trajectory = slam.getTrajectoryEstimate()
+    t_real = toc;
+    if (t_real - t_plot_update > 1)
+        % Get results
+        local_map = slam.getMapEstimate();
+        %scatter(local_map(:,2),local_map(:,3));
+        local_map_scatter.XData = local_map(:,2);
+        local_map_scatter.YData = local_map(:,3);
+        trajectory = slam.getTrajectoryEstimate();
+        t_plot_update = toc;
+    end
 
     % TODO: Perform icp on here_map and local_map to calculate map_transform
 
-    % TODO: Visualize trajectory estimate, local_map and here_map
+    % Visualize trajectory
+    pose = slam.getPoseEstimate();
+    addpoints(trajectory_line, pose.x, pose.y);
+    drawnow limitrate nocallbacks;
+    % TODO: Visualize local_map and here_map
 
     % Get the next time value
-    next_t_odometry = find(t_odometry>t,1)
-    next_t_gps = find(t_gps>t,1)
-    next_t_landmarks = find(t_landmarks>t,1)
-    t = min([next_t_odometry, next_t_gps, next_t_landmarks])
-    if (size(t,1) == 0)
+    next_t_odometry = t_odometry(i_odometry);
+    next_t_gps = [];
+    next_t_landmarks = t_landmarks(i_landmarks);
+    t = min([next_t_odometry; next_t_gps; next_t_landmarks]);
+    if (t >= END_TIME)
         done = 1;
     end
 end
